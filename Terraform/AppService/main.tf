ada5af35a0c57d25375c75b1c61be67487cc26ef
rudra292 <srudra.cs1@gmail.com>
2025-10-29 16:17:47 +0530

    Remove empty line in main.tf

--- Terraform/AppService/main.tf
+++ Terraform/AppService/main.tf
@@ -4,7 +4,6 @@ data "azurerm_resource_group" "example" {
 }
 
 
-
 resource "azurerm_app_service_plan" "example" {
   name                = var.app_service_plan_name
   location            = data.azurerm_resource_group.example.location
