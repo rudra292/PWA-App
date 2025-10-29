50b1b82094d33d325b866704f831ae8d7ef2e40a
rudra292 <srudra.cs1@gmail.com>
2025-10-29 15:47:18 +0530

    Create main.tf

---
+++ Terraform/AppService/main.tf
@@ -0,0 +1,30 @@
+
+data "azurerm_resource_group" "example" {
+  name = var.resource_group_name
+}
+
+
+
+resource "azurerm_app_service_plan" "example" {
+  name                = var.app_service_plan_name
+  location            = data.azurerm_resource_group.example.location
+  resource_group_name = data.azurerm_resource_group.example.name
+  sku {
+    tier = "Standard"
+    size = "S1"
+  }
+}
+
+resource "azurerm_app_service" "webapp" {
+  name                = var.app_service_name
+  location            = data.azurerm_resource_group.example.location
+  resource_group_name = data.azurerm_resource_group.example.name
+  app_service_plan_id = azurerm_app_service_plan.example.id
+
+  site_config {
+    always_on        = true
+    linux_fx_version = var.linux_fx_version
+  }
+
+  https_only = true
+}
