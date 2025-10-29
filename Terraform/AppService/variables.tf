1f04e630656571089be913d4e5f2093432f24805
rudra292 <srudra.cs1@gmail.com>
2025-10-29 15:48:59 +0530

    Add variables for App Service configuration

---
+++ Terraform/AppService/variables.tf
@@ -0,0 +1,67 @@
+variable "resource_group_name" {
+  description = "Name of the resource group"
+  type        = string
+  default = "rg-dev-app"
+}
+
+variable "app_service_plan_name" {
+  description = "Name of the App Service Plan"
+  type        = string
+  default = "my-app-service-plan"
+}
+
+variable "app_service_name" {
+  description = "Name of the App Service (Web App)"
+  type        = string
+  default     = "webpulse-dev1028"
+}
+
+variable "linux_fx_version" {
+  description = "Runtime stack for Linux (e.g., PYTHON|3.10, NODE|18-lts)"
+  type        = string
+  default     = "PYTHON|3.10"
+}
+/*
+#--------------plan variables---------------------------
+
+# variable "resource_group_name" {
+#   description = "Name of the existing resource group"
+#   type        = string
+#   default = "rg-dev-app"
+# }
+
+# variable "app_service_plan_name" {
+#   description = "Name of the App Service Plan"
+#   type        = string
+#   default     = "my-app-service-plan"
+# }
+
+# variable "kind" {
+#   description = "Type of App Service (Windows or Linux)"
+#   type        = string
+#   default     = "Linux"
+# }
+*/
+variable "reserved" {
+  description = "Set to true for Linux plans"
+  type        = bool
+  default     = true
+}
+/*
+# variable "sku_tier" {
+#   description = "Pricing tier (e.g., Free, Basic, Standard, Premium)"
+#   type        = string
+#   default     = "Basic"
+# }
+*/
+variable "sku_size" {
+  description = "Instance size (e.g., B1, S1, P1v2)"
+  type        = string
+  default     = "B1"
+}
+
+variable "os_type" {
+  description = "Operating system for the App Service Plan (Linux or Windows)"
+  type        = string
+  default     = "Linux"
+}
